30efa98e6fc9797c0a89e3803db260f19b27e08e
vladpotuk <vladpotuk01@gmail.com>
2024-02-20 19:34:28 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введіть початок діапазону: ");
+        int start = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Введіть кінець діапазону: ");
+        int end = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine($"Числа Фібоначчі в діапазоні від {start} до {end}:");
+
+        
+        PrintFibonacciNumbersInRange(start, end);
+    }
+
+    
+    static void PrintFibonacciNumbersInRange(int start, int end)
+    {
+        int a = 0;
+        int b = 1;
+        int c = a + b;
+
+        while (c <= end)
+        {
+            if (c >= start)
+            {
+                Console.Write($"{c} ");
+            }
+            a = b;
+            b = c;
+            c = a + b;
+        }
+
+        Console.WriteLine(); 
+    }
+}
